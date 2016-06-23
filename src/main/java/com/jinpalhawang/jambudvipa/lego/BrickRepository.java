a7f069434d9176e5ccfbed0efe7cdc590f89fc2d
Nicholas Eden-Walker <nedenwalker@kenzan.com>
2016-06-23 17:05:06 -0400

    Added find multiple ids to brick repo.

--- src/main/java/com/jinpalhawang/jambudvipa/lego/BrickRepository.java
+++ src/main/java/com/jinpalhawang/jambudvipa/lego/BrickRepository.java
@@ -1,8 +1,14 @@
 package com.jinpalhawang.jambudvipa.lego;
 
+import java.util.List;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface BrickRepository extends MongoRepository<Brick, ObjectId> {
+
+  public List<Brick> findByIdIn(List<ObjectId> ids);
+
+  public List<Brick> findByElementIdIn(List<String> elementIds);
 
 }
--- src/main/java/com/jinpalhawang/jambudvipa/lego/DatabaseLoader.java
+++ src/main/java/com/jinpalhawang/jambudvipa/lego/DatabaseLoader.java
@@ -1,5 +1,9 @@
 package com.jinpalhawang.jambudvipa.lego;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -17,20 +21,33 @@ public class DatabaseLoader implements CommandLineRunner {
   @Override
   public void run(String... args) throws Exception {
 
-    this.repository.deleteAll();
+    repository.deleteAll();
 
-    this.repository.save(new Brick("Corner Plate 1x2x2", "Plates", "Red", "242021"));
-    this.repository.save(new Brick("Corner Plate 1x2x2", "Plates", "Black", "242021"));
-    this.repository.save(new Brick("Corner Plate 1x2x2", "Plates", "Blue", "242021"));
-    this.repository.save(new Brick("Corner Plate 1x2x2", "Plates", "Yellow", "242021"));
-    this.repository.save(new Brick("Corner Plate 1x2x2", "Plates", "Green", "242021"));
-    this.repository.save(new Brick("Corner Plate 1x2x2", "Plates", "Pink", "242021"));
-    this.repository.save(new Brick("Corner Plate 1x2x2", "Plates", "Purple", "242021"));
+    repository.save(new Brick("Corner Plate 1x2x2", "Plates", "White", "242001"));
+    repository.save(new Brick("Corner Plate 1x2x2", "Plates", "Red", "242021"));
+    repository.save(new Brick("Corner Plate 1x2x2", "Plates", "Blue", "242023"));
+    repository.save(new Brick("Corner Plate 1x2x2", "Plates", "Yellow", "242024"));
+    repository.save(new Brick("Corner Plate 1x2x2", "Plates", "Black", "242026"));
+    repository.save(new Brick("Corner Plate 1x2x2", "Plates", "Grey", "4210635"));
+
+    List<ObjectId> ids = new ArrayList<ObjectId>();
 
     System.out.println("\nBricks found with findAll():");
     for (Brick brick : repository.findAll()) {
       System.out.println(brick);
+      ids.add(brick.getId());
     }
+
+    System.out.println("\nBricks found with findByIdIn(ids):");
+    ids.remove(0);
+    ids.remove(0);
+    System.out.println(repository.findByIdIn(ids));
+
+    System.out.println("\nBricks found with findByElementIdIn(elementIds):");
+    List<String> elementIds = new ArrayList<String>();
+    elementIds.add("242021");
+    elementIds.add("242026");
+    System.out.println(repository.findByElementIdIn(elementIds));
     System.out.println();
   }
 
